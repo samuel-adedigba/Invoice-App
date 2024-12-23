0ef67ff6cdfdfa704c4a5d90cd3a28f76c8f37c8
Adedigba <samueladedigba83@gmai.com>
2024-12-23 11:36:28 +0000

    first commit

---
+++ backend/node_modules/@novu/shared/dist/cjs/utils/slugify/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.slugify = void 0;
+var slugify_1 = require("./slugify");
+Object.defineProperty(exports, "slugify", { enumerable: true, get: function () { return slugify_1.slugify; } });
